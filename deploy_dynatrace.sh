41ca7b67d6bc86a46558321b9905d3d9c2a2a59d
Florian Bacher <florian.bacher@dynatrace.com>
2021-04-14 16:12:07 +0200

    deploy oneagent, create tagging rules
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- deploy_dynatrace.sh
+++ deploy_dynatrace.sh
@@ -1,4 +1,74 @@
 #!/bin/bash
 
-kubectl get namespaces # TODO remove, only for testing purposes
-#wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "${DT_TENANT}" --api-token "${DT_API_TOKEN}" --paas-token "${DT_PAAS_TOKEN}" --enable-k8s-monitoring --enable-volume-storage
+wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "${DT_TENANT}" --api-token "${DT_API_TOKEN}" --paas-token "${DT_PAAS_TOKEN}" --enable-k8s-monitoring --enable-volume-storage
+
+# setup automatic tagging rules
+
+# keptn_selfmon_service tagging rule, e.g., keptn_selfmon_service: shipyard-controller
+
+curl -X POST \
+  https://${DT_TENANT}/api/config/v1/autoTags \
+  -H "Authorization: Api-token ${DT_API_TOKEN}" \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "name": "keptn_selfmon_service",
+  "rules": [
+        {
+            "type": "SERVICE",
+            "enabled": true,
+            "valueFormat": "{ProcessGroup:KubernetesContainerName}",
+            "propagationTypes": [],
+            "conditions": [
+                {
+                    "key": {
+                        "attribute": "PROCESS_GROUP_PREDEFINED_METADATA",
+                        "dynamicKey": "KUBERNETES_NAMESPACE",
+                        "type": "PROCESS_PREDEFINED_METADATA_KEY"
+                    },
+                    "comparisonInfo": {
+                        "type": "STRING",
+                        "operator": "BEGINS_WITH",
+                        "value": "'${KEPTN__SELF_MONITORING_PROJECT}'",
+                        "negate": false,
+                        "caseSensitive": true
+                    }
+                }
+            ]
+        }
+    ]
+}
+'
+
+# keptn_selfmon_stage tagging rule, e.g., keptn_selfmon_stage: tanus-dev
+curl -X POST \
+  https://${DT_TENANT}/api/config/v1/autoTags \
+  -H "Authorization: Api-token ${DT_API_TOKEN}" \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "name": "keptn_selfmon_service",
+  "rules": [
+        {
+            "type": "SERVICE",
+            "enabled": true,
+            "valueFormat": "{ProcessGroup:KubernetesNamespace}",
+            "propagationTypes": [],
+            "conditions": [
+                {
+                    "key": {
+                        "attribute": "PROCESS_GROUP_PREDEFINED_METADATA",
+                        "dynamicKey": "KUBERNETES_NAMESPACE",
+                        "type": "PROCESS_PREDEFINED_METADATA_KEY"
+                    },
+                    "comparisonInfo": {
+                        "type": "STRING",
+                        "operator": "BEGINS_WITH",
+                        "value": "'${KEPTN__SELF_MONITORING_PROJECT}'",
+                        "negate": false,
+                        "caseSensitive": true
+                    }
+                }
+            ]
+        }
+    ]
+}
+'
