96fb5b546e7c49d1c3fc86b696db935973a836eb
Florian Bacher <florian.bacher@dynatrace.com>
2021-04-15 09:03:19 +0200

    fix dt api url
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- deploy_dynatrace.sh
+++ deploy_dynatrace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "${DT_TENANT}" --api-token "${DT_API_TOKEN}" --paas-token "${DT_PAAS_TOKEN}" --enable-k8s-monitoring --enable-volume-storage
+wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "https://${DT_TENANT}/api" --api-token "${DT_API_TOKEN}" --paas-token "${DT_PAAS_TOKEN}" --enable-k8s-monitoring --enable-volume-storage
 
 # setup automatic tagging rules
 
