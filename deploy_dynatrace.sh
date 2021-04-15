b44344a7ad3fa2976e7323f2e542cb7df33bf6aa
Florian Bacher <florian.bacher@dynatrace.com>
2021-04-15 17:31:48 +0200

    setup dynatrace monitoring for keptn
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- deploy_dynatrace.sh
+++ deploy_dynatrace.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+source ${{ github.action_path }}/utils.sh
+
+echo "Deploying Dynatrace OneAgent Operator"
 wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh -O install.sh && sh ./install.sh --api-url "https://${DT_TENANT}/api" --api-token "${DT_API_TOKEN}" --paas-token "${DT_PAAS_TOKEN}" --enable-k8s-monitoring --enable-volume-storage
 
 # setup automatic tagging rules
 
 # keptn_selfmon_service tagging rule, e.g., keptn_selfmon_service: shipyard-controller
-
+echo "setting up tagging rule: keptn_selfmon_service"
 curl -X POST \
   https://${DT_TENANT}/api/config/v1/autoTags \
   -H "Authorization: Api-token ${DT_API_TOKEN}" \
@@ -39,13 +42,14 @@ curl -X POST \
 }
 '
 
-# keptn_selfmon_stage tagging rule, e.g., keptn_selfmon_stage: tanus-dev
+# keptn_selfmon_namespace tagging rule, e.g., keptn_selfmon_namespace: tanus-dev
+echo "setting up tagging rule: keptn_selfmon_namespace"
 curl -X POST \
   https://${DT_TENANT}/api/config/v1/autoTags \
   -H "Authorization: Api-token ${DT_API_TOKEN}" \
   -H 'Content-Type: application/json' \
   -d '{
-  "name": "keptn_selfmon_stage",
+  "name": "keptn_selfmon_namespace",
   "rules": [
         {
             "type": "SERVICE",
@@ -72,3 +76,22 @@ curl -X POST \
     ]
 }
 '
+
+echo "creating Dynatrace secret"
+kubectl create secret generic dynatrace -n "keptn" --from-literal="DT_TENANT=${DT_TENANT}" --from-literal="DT_API_TOKEN=${DT_API_TOKEN}"
+
+echo "deploying dynatrace-service"
+kubectl apply -f https://github.com/keptn-contrib/dynatrace-service/blob/release-0.12.0/deploy/service.yaml -n keptn
+
+echo "deploying dynatrace-sli-service"
+kubectl apply -f https://github.com/keptn-contrib/dynatrace-sli-service/blob/release-0.9.0/deploy/service.yaml -n keptn
+
+wait_for_deployment_in_namespace "dynatrace-service" "keptn"
+wait_for_deployment_in_namespace "dynatrace-sli-service" "keptn"
+
+echo "configuring keptn for dynatrace monitoring"
+keptn configure monitoring dynatrace --project="${KEPTN_SELF_MONITORING_PROJECT}"
+
+echo "uploading sli config to ${KEPTN_SELF_MONITORING_PROJECT}"
+keptn add-resource --project=${KEPTN_SELF_MONITORING_PROJECT} --service=keptn --resource=${{ github.action_path }}/assets/sli.yaml --resourceUri=sli.yaml --all-stages
+keptn add-resource --project=${KEPTN_SELF_MONITORING_PROJECT} --service=keptn --resource=${{ github.action_path }}/assets/slo.yaml --resourceUri=slo.yaml --all-stages
